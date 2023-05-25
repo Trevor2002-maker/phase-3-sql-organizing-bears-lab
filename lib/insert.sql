20bc8f47b54b4ce816e9da556d4cc878fba0de2e
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2023-05-25 18:39:33 +0300

    commit tree d8cdda190c718858f9cf2ea22ce2a0ac7f2bbdac

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', NULL, 'M', NULL, NULL, true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', NULL, 'M', NULL, NULL, true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', NULL, 'F', NULL, NULL, true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', NULL, 'M', NULL, NULL, true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', NULL, 'F', NULL, NULL, true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', NULL, 'M', NULL, NULL, true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', NULL, 'F', NULL, NULL, true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, NULL, NULL, NULL, NULL, true);
